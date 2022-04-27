6151d808445626fdb9ebbbd4a2c0b0337fc49a80
Daniel Kevin Lopes de Mello <daniel.mello@geen.com.br>
2022-04-27 16:55:25 -0300

    Update Program.cs

--- Questao3/Program.cs
+++ Questao3/Program.cs
@@ -3,92 +3,89 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
-/// <summary>
-/// Resumo:
-///     Escreve no console o passo a passo da preparação da pipoca,
-///     desde o recebimento do pedido até ser finalizado. 
-/// </summary>
-static void getPipoca()
+
+class Program
 {
-    Console.WriteLine($"Pedido de pipoca recebido!");
-    Console.WriteLine($"Preparando pipoca em 6 segundos...");
+    static void Main(string[] args)
+    {
+        // Fila de clientes do cinema
+        var fila = new List<string>
+        {
+            "Angela",
+            "Bruno",
+            "Celeste",
+            "Duda",
+            "Edu",
+            "Fabio",
+            "Geovana",
+            "Hercules"
+        };
 
-    // Colocando a thread em espera por 6s
-    Thread.Sleep(6000);
+        // Simulando o caixa do cinema recebendo os pedidos dos clientes, iniciando e finalizando
+        foreach (var cliente in fila)
+        {
+            Console.WriteLine($"------Pedido de {cliente} iniciado!------");
+            FazerPedido(cliente);
+            Console.WriteLine($"------Pedido de {cliente} finalizado!------\n\n");
 
-    Console.WriteLine($"Pipoca pronta!");
+        }
+    }
+
+    /// <summary>
+    /// Resumo:
+    ///     Função onde recebe o nome do cliente, faz a chamada da preparação do pedido e
+    ///     aguarda até que os 2 fiquem prontos
+    /// </summary>
+    public static void FazerPedido(string nome)
+    {
+        IniciarPreparacao();
+        Console.WriteLine($"Pedido de {nome} está pronto!!");
+    }
+
+    /// <summary>
+    /// Resumo: 
+    ///     Função usada para chamar a preparação da pipoca e refrigerante.    
+    /// </summary>
+    public static void IniciarPreparacao()
+    {
+        // Cria 2 novas tarefas, uma com o metodo getPipoca, outra com o metodo getRefrigerante.
+        var taskPipoca = Task.Run(() => GetPipoca());
+        var taskRefri = Task.Run(() => GetRefigerante());
+
+        // Realiza a espera das 2 tarefas terminarem para continuar a execução
+        Task.WaitAll(taskPipoca, taskRefri);
+    }
+
+
+    /// <summary>
+    /// Resumo:
+    ///     Escreve no console o passo a passo da preparação da pipoca,
+    ///     desde o recebimento do pedido até ser finalizado. 
+    /// </summary>
+    public static void GetPipoca()
+    {
+        Console.WriteLine($"Pedido de pipoca recebido!");
+        Console.WriteLine($"Preparando pipoca em 6 segundos...");
+
+        // Colocando a thread em espera por 6s
+        Thread.Sleep(6000);
+
+        Console.WriteLine($"Pipoca pronta!");
+    }
+
+    /// <summary>
+    /// Resumo:
+    ///     Escreve no console o passo a passo da preparação do refrigerante,
+    ///     desde o recebimento do pedido até ser finalizado.
+    /// </summary>
+    public static void GetRefigerante()
+    {
+        Console.WriteLine($"Pedido de refrigerante recebido!");
+        Console.WriteLine($"Preparando refrigerante em 2.5 segundos...");
+
+        // Colocando a thread em espera por 2.5s
+        Thread.Sleep(2500);
+
+        Console.WriteLine($"Refrigerante pronto!");
+    }
 }
-
-/// <summary>
-/// Resumo:
-///     Escreve no console o passo a passo da preparação do refrigerante,
-///     desde o recebimento do pedido até ser finalizado.
-/// </summary>
-static void getRefigerante()
-{
-    Console.WriteLine($"Pedido de refrigerante recebido!");
-    Console.WriteLine($"Preparando refrigerante em 2.5 segundos...");
-
-    // Colocando a thread em espera por 2.5s
-    Thread.Sleep(2500);
-
-    Console.WriteLine($"Refrigerante pronto!");
-}
-
-/// <summary>
-/// Resumo: 
-///     Função usada para chamar a preparação da pipoca e refrigerante.    
-/// </summary>
-void IniciarPreparacao()
-{
-    // Cria 2 novas tarefas, uma com o metodo getPipoca, outra com o metodo getRefrigerante.
-    var taskPipoca = new Task(() => getPipoca());
-    var taskRefri = new Task(() => getRefigerante());
-
-    // Inicia as 2 tarefas com o metodo Start()
-    taskPipoca.Start();
-    taskRefri.Start();
-
-    // Realiza a espera das 2 tarefas terminarem para continuar a execução
-    Task.WaitAll(taskPipoca, taskRefri);
-}
-
-/// <summary>
-/// Resumo:
-///     Função onde recebe o nome do cliente, faz a chamada da preparação do pedido e
-///     aguarda até que os 2 fiquem prontos
-/// </summary>
-void FazerPedido(string nome)
-{
-    IniciarPreparacao();
-    Console.WriteLine($"Pedido de {nome} está pronto!!");
-}
-
-/// <summary>
-/// Resumo:
-///     Fila de clientes do cinema
-/// </summary>
-var fila = new List<string>
-{
-    "Angela",
-    "Bruno",
-    "Celeste",
-    "Duda",
-    "Edu",
-    "Fabio",
-    "Geovana",
-    "Hercules"
-};
-
-/// <summary>
-/// Resumo:
-///     Simulando o caixa do cinema recebendo os pedidos dos clientes, iniciando e finalizando
-/// </summary>
-foreach (var cliente in fila)
-{
-    Console.WriteLine($"------Pedido de {cliente} iniciado!------");
-    FazerPedido(cliente);
-    Console.WriteLine($"------Pedido de {cliente} finalizado!------\n\n");
-
-}
-
